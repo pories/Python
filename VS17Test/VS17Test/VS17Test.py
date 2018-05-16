058490cbb84b3b155c4d40beefdf7055525e1992
Michael Pories <pories@hotmail.com>
2018-05-16 19:07:55 -0400

    Added info for basic math.

--- VS17Test/VS17Test/VS17Test.py
+++ VS17Test/VS17Test/VS17Test.py
@@ -7,4 +7,17 @@ print("Hello World")
 '''
 multiline comments
 '''
-
+var1 = "Michael"
+print(var1)
+#single or double quotes
+#variable stores values. Has to start with a letter, then it can numbers or _.
+# Five main data types in python: numbers, strings, lists, tuples, dictionaries (maps)
+# Seven math types: +,-,*,/,%, ** (exponential), and // (floor devision, discards remainder and rotates down. 14.5 goes to 14.)
+#print("5+2= ", 5+2 )
+#print("5-2= ", 5-2)
+#print("5*2= ", 5*2)
+#print("5/2= ", 5/2)
+#print("5%2= ", 5%2)
+#print("5**2= ", 5**2)
+#print("5//2= ", 5//2)
+#Mathmatical order of operation is pemdas
